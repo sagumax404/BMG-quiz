0eb6468365888f4bbae6393aaaa78e79a83056f5
Samet Gümüş <90527001+sagumax404@users.noreply.github.com>
2021-12-31 13:43:06 +0300

    Update quiz1.py

--- quiz1.py
+++ quiz1.py
@@ -1,3 +1,5 @@
+#"il" adlı değişkene herhangi bir il adlı girildiğinde bu ilin ilk harfini ve sonrasında aradaki her harf kadar "*" işaret ve ilin son harfini basacak şekilde print ifadesinin 
+#içinde boş bırakılan yeri doldurunuz. Örneğin il istanbul girilmişse i******l  yazacak fakat il bursa girilmişse b***a yazacaktır. 
 il="Ankara"
 yıldız="*"*(len(il)-2)
 
