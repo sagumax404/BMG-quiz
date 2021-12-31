ae7abbcf4c10e95c95f1f1f77569a2bcdc66f0b9
Samet Gümüş <90527001+sagumax404@users.noreply.github.com>
2021-12-31 17:11:48 +0300

    Update quiz4.py

--- quiz4.py
+++ quiz4.py
@@ -1,4 +1,10 @@
+"""
+Kullanıcıdan alacağınız x ve y değerlerine karşılık gelen (x,y) noktasının "r" yarıçağlı çemberin içinde olup olmadığını kontrol  eden python fonksiyonu yazınız. 
+Eğer verilen nokta çemberin içindeyse 1 sonucunu, değilse 0 sonucunu döndürecektir.
+Not: Sadece fonksiyonu yazmanız yeterlidir.  Fonksiyon şu değişkenleri alacak ve şu adlı olacaktır.
 
+cemberkontrol( r , x , y )
+"""
 def cemberkontrol( r , x , y ):
     uzaklık=(x*x+y*y)**0.5
     if(uzaklık==5):
