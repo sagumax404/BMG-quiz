66777e38d372580ca7ed079bfa8f430fc6568cd9
Samet Gümüş <90527001+sagumax404@users.noreply.github.com>
2021-12-31 17:10:14 +0300

    Update quiz3.py

--- quiz3.py
+++ quiz3.py
@@ -1,3 +1,6 @@
+"""
+Kullanıcının girdiği bir değere göre çarpım tablosu oluşturan programı kodlayınız. 
+"""
 sayi = int(input("Kaçlar tablosu yapılacak:"))
 for s in range(0,11):
     samet = sayi*s
